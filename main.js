d2cd735a5e7b99876cb87318ae3a3ba09d6040ce
Dilsher Balouch <dilsherb@gmail.com>
2022-12-21 20:06:13 +0300

    linter issues resolved

--- main.js
+++ main.js
@@ -6,17 +6,3 @@ const bsCollapse = new bootstrap.Collapse(menuToggle);
 navLinks.forEach((n) => {
   n.addEventListener('click', () => { bsCollapse.toggle(); });
 });
-
-// modal started
-// getting data from HTML
-const project1 = {
-    name: document.querySelector('#title1').innerHTML,
-    img: document.querySelector('#imgProject1').src,
-}
-// looping through data
-data = "";
-for (let project in project1){
-    data +=project1[project]+"<br>";
-};
-// showing data to modal
-document.getElementById("body1").innerHTML = data;
